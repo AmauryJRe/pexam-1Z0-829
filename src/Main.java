687c16ce929d3f3c0b9c1dadc98bcbd538b88a03
Amaury Jorge Reynaldo <amauryjreynaldo@gmail.com>
2024-08-10 21:57:03 -0400

    side-effect

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // % and module are not the same, % is remainder,
+        // this takes the sign from the left and ignores the sign on the right
+        int y = -2%-3;
+
+        // Any random number generated with Math.random, minus a number can lead to negative results
+        // since random generated numbers can result in 0 or smaller positive number that the one substracted
+        int x=(int)(Math.random()*6)-3;
+        System.out.println(y);
     }
 }
