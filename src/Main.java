5856c3d6038be0d0e332dffb009dc26d4a8eb9dd
Amaury Jorge Reynaldo <amauryjreynaldo@gmail.com>
2024-08-14 20:42:43 -0400

    Handling text

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println('\u7231');
+        String s1 = "Hello";
+        String s2 = """
+                Hello""";
+        // String s3 = """Hello""";
+        String s4 = """
+                Hello
+                """;
+        String s5 = """
+                             Hello
+                """;
+        String s6 = """
+                Hello\r\n""";
+        String s7 = """
+                "\"""";
+        System.out.println(s7);
+        System.out.println('\u7231');
+
+        int i = 0;
+        String s = "VAlues: ";
+        loop:
+        while (i < 5) {
+            if (i++ % 2 == 0) continue;
+            System.out.println("Continued loop: " + i);
+            s.concat(", ").concat(Integer.toString(i));
+        }
+        System.out.println(s);
+
     }
+
 }
