76341ff52371a7b2fd0a8699792f7c22b7a18442
Amaury Jorge Reynaldo <amauryjreynaldo@gmail.com>
2024-10-06 09:37:24 -0400

    not know what happend

--- src/deepdive/ONE.java
+++ src/deepdive/ONE.java
@@ -1,4 +1,17 @@
 package deepdive;
 
 public class ONE {
+    static int x;
+    int y;
+    {x++;y++;}
+    static {x++;}
+    ONE(){this(++x);}
+    ONE(int x){
+        System.out.println(x+", "+y);
+    }
+    static {new ONE();}
+
+    public static void main(String[] args) {
+
+    }
 }
--- src/instanceinitializations/TryIt.java
+++ src/instanceinitializations/TryIt.java
@@ -1,4 +1,30 @@
 package instanceinitializations;
 
 public class TryIt {
+    public static void main(String[] args) {
+        new TryIt().go();
+    }
+
+    public void go() {
+        System.out.println(STR."\{x}, \{y}, \{z}");
+    }
+
+    int x;
+
+    {
+        System.out.println(STR."\{x}, \{this.y}");
+    }
+
+    int y = 100;
+
+    {
+
+        System.out.println(STR."\{x}, \{this.y}");
+    }
+
+    {
+        x = y + 10;
+    }
+
+    int z = -1;
 }
